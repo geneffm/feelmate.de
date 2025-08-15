41e58ef2bd7ecbcf1a7a1ba5f57bcdc4789bf86a
geneffm <feelmate.de@gmail.com>
2025-08-15 19:21:16 +0200

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-function openModal(id){ document.getElementById(id).classList.remove('hidden'); }
-function closeModal(id){ document.getElementById(id).classList.add('hidden'); }
