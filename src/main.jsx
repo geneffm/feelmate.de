e473bbceddb354db93069f0718018b9bda0de2f8
geneffm <feelmate.de@gmail.com>
2025-08-16 20:38:15 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+
+// Seiten
 import App from './pages/App.jsx'
+import Impressum from './pages/Impressum.jsx'
+import Datenschutz from './pages/Datenschutz.jsx'
+import AGB from './pages/AGB.jsx'
+import Widerruf from './pages/Widerruf.jsx' // falls schon angelegt
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/impressum" element={<Impressum />} />
+        <Route path="/datenschutz" element={<Datenschutz />} />
+        <Route path="/agb" element={<AGB />} />
+        <Route path="/widerruf" element={<Widerruf />} />
+        {/* Fallback (optional): */}
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
